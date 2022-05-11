5d5b0985b4046e4429075de00c73713f8c31a543
geogour <geogour@gmail.com>
2022-05-11 16:13:35 +0300

    s1

---
+++ extract_zip_files_in_directories.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import zipfile
+
+root_dir = Path('.')
+destination = Path('destination')
+
+for path in root_dir.glob("*.zip"):
+  with zipfile.ZipFile(path, 'r') as zf:
+    final_path = destination / Path(path.stem)
+    zf.extractall(final_path)
