6c8655273d9fa0a36a457c3443543981c5299331
geogour <geogour@gmail.com>
2022-05-11 14:50:15 +0300

    s1

---
+++ compress_files.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from zipfile import ZipFile
+
+root_dir = Path('files')
+
+with ZipFile('my_python_files.zip','w') as zip:
+        # writing each file one by one
+        for file in root_dir.rglob("*.txt"):
+          zip.write(file)
+  
+    
+print('All files zipped successfully!') 
